9a27acf9aa0404456d93f18690ba0c503bc7cfd3
waheb-benzaid <waheb.benzaid@gmail.com>
2023-12-08 19:09:28 +0100

    create a new api

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,6 +1,7 @@
 import requests
 
-endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/api/"
 
 get_response = requests.get(endpoint,json ={"query":"Hello World"})
 print(get_response.text)
+print(get_response.status_code)
